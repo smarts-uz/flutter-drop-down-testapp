cdb0b0f1a5741e47e59a938b3e74ce70392189f7
Mukhtor <muxtoreshimov11@gmail.com>
2023-11-18 17:21:55 +0500

    tested multiple_search_selection

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'multiple_search_selection.dart';
 import 'multiselect.dart';
 
 void main() {
@@ -13,8 +14,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Flutter Demo',
-      home: MultiSelectPage(
-        title: 'MultiSelect Package',
+      home: MultipleSearchSelectionPage(
+        title: 'MultipleSearchSelection',
       ),
     );
   }
